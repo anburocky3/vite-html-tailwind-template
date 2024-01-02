e4c2dbec79f080e0fadd9cf146d2f5ffef7bace2
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-02 15:37:11 +0530

    init

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  // base: '/YOUR_BASE/'
+  //   build: {
+  //     rollupOptions: {
+  //       input: {
+  //         // main: resolve(__dirname, "index.html"),
+  //       },
+  //     },
+  //   },
+});
